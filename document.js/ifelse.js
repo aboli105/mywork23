b8648620db447bc92274b81e1114093cb143511f
aboli105 <aboswaghmare125@gmail.com>
2023-09-10 13:32:45 +0530

    first commit

---
+++ document.js/ifelse.js
@@ -0,0 +1,15 @@
+let numberOfpen = 5
+
+if (numberOfpen > 9) {
+document.write("welldone!")
+}
+else{
+document.write("fail")
+}
+
+//>>let inside the () is condition 
+//operators 
+
+// > gretaer than 
+// < lesser than
+// == equal to equal to 
