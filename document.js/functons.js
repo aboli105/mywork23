f98ac2d7fbb3be7beef9c6ac23d90ceac8f2cfb1
aboli105 <aboswaghmare125@gmail.com>
2023-09-10 14:40:55 +0530

    new

---
+++ document.js/functons.js
@@ -0,0 +1,13 @@
+//higher order function
+
+//() is use for call the function
+
+document.addEventListener("click", ourFunction)
+
+function ourFunction(){
+   alert("Thank you")
+}
+  
+/*A higher order function is a Function that either :
+ (A) Accepts a function as an argument
+ (B) Retuns as a function /*
